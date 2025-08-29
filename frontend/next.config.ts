c98bd4ff21c15d979bc8f700670db5843424c28a
wuoc <quockhanh4114@gmail.com>
2025-08-29 23:32:39 +0700

    chore: update

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    optimizeCss: false,
-  },
+  reactStrictMode: true,
+  swcMinify: true,
+  optimizeCss: false,
 };
 
 export default nextConfig;
